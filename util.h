99ee7867707142f9356f40e5d87f30e35c2abc6a
toririm <longtimedragon@icloud.com>
2025-08-27 00:33:58 +0900

    add read_from_file

--- util.h
+++ util.h
@@ -1,6 +1,16 @@
+#include <omp.h>
 
-#define max(x, y) (x >= y ? x : y)
+#define MAX(a, b) ((a) >= (b) ? (a) : (b))
+#define MIN(a, b) ((a) <  (b) ? (a) : (b))
+#define MEASURE(label, code) \
+    do { \
+        double __st = omp_get_wtime(); \
+        code \
+        double __en = omp_get_wtime(); \
+        printf("[TIME] %s: %lf sec\n", #label, __en - __st); \
+    } while(0)
 
+extern char *read_from_file(char *filepath);
 extern void gaussian_random_vec(int n, double *r);
 extern double dot_product(double *a, double *b, int size);
 extern void diagonalize_double(double **symmetric_matrix, double *eigenvalues, double **eigenvectors, int n);
