520391ad336b24baf1abc2082728448ac40267a0
toririm <longtimedragon@icloud.com>
2025-09-02 00:00:25 +0900

    feat: selectable matrix format

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <omp.h>
 #include "main.h"
@@ -12,40 +13,50 @@
 
 int main(int argc, char *argv[]) {
 	char *filename;
+	Mat_Type mat_type;
 	Mat_Coo mat;
 	Mat_Crs mat_crs;
 	double eigenvalues[buf_size];
 	double *eigenvectors[buf_size];
 
-	if (argc != 2) {
-		fprintf(stderr, "Usage: %s <input_file>\n", argv[0]);
+	if (argc != 3) {
+		fprintf(stderr, "Usage: %s <input_file> <coo|crs>\n", argv[0]);
 		return EXIT_FAILURE;
 	}
 	filename = argv[1];
+	if (strcmp(argv[2], "coo") == 0) {
+		mat_type = COO;
+	} else if (strcmp(argv[2], "crs") == 0) {
+		mat_type = CRS;
+	} else {
+		fprintf(stderr, "Usage: %s <input_file> <coo|crs>\n", argv[0]);
+		return EXIT_FAILURE;
+	}
 
 	MEASURE(read_mat,
 		mat = read_mat_coo(filename);
 	);
 
-
 	printf("mat dim: %d\n", mat.dimension);
-	
-	for (int i = 0; i < number_of_eigenvalues; i++) {
-		eigenvectors[i] = calloc(mat.dimension, sizeof(double));
+
+	if (mat_type == CRS) {
+		printf("[MODE] CRS selected\n");
+		// Fortran から出力されたファイルは 列優先 (Column Major) なので、CRSとして読み込むためソートする
+		MEASURE(sort_matcoo,
+			sort_matcoo(&mat);
+		);
+		MEASURE(convert_from_coo,
+			mat_crs = convert_from_coo(&mat);
+		);
+		MEASURE(lanczos,
+			lanczos(MAKE_MAT_MATVEC(&mat_crs), eigenvalues, eigenvectors, number_of_eigenvalues, 100, 10e-5);
+		);
+	} else {
+		printf("[MODE] COO selected\n");
+		MEASURE(lanczos,
+			lanczos(MAKE_MAT_MATVEC(&mat), eigenvalues, eigenvectors, number_of_eigenvalues, 100, 10e-5);
+		);
 	}
-
-	// Fortran から出力されたファイルは 列優先 (Column Major) なので、CRSとして読み込むためソートする
-	MEASURE(sort_matcoo,
-		sort_matcoo(&mat);
-	);
-
-	MEASURE(convert_from_coo,
-		mat_crs = convert_from_coo(&mat);
-	);
-
-	MEASURE(lanczos,
-		lanczos(MAKE_MAT_MATVEC(&mat_crs), eigenvalues, eigenvectors, number_of_eigenvalues, 100, 10e-5);
-	);
 
 	for (int i = 0; i < number_of_eigenvalues; i++) {
 		printf("eigenvalue\t\t%d\t%.12f\n", i + 1, eigenvalues[i]);
--- src/main.h
+++ src/main.h
@@ -1,1 +1,6 @@
+typedef enum mat_type {
+    COO,
+    CRS,
+} Mat_Type;
+
 extern int main(int argc, char *argv[]);
