b759f4fa0416a8c210c37d60a92ef4aa7eb2647e
toririm <longtimedragon@icloud.com>
2025-09-01 21:22:26 +0900

    fix: MEASURE macro

--- src/util.h
+++ src/util.h
@@ -1,13 +1,17 @@
+#include <stdio.h>
 #include <omp.h>
 
 #define MAX(a, b) ((a) >= (b) ? (a) : (b))
 #define MIN(a, b) ((a) <  (b) ? (a) : (b))
+/**
+ * 時間計測をするマクロ
+ */
 #define MEASURE(label, code) \
     do { \
         double __st = omp_get_wtime(); \
         code \
         double __en = omp_get_wtime(); \
-        printf("[TIME] %s: %lf sec\n", #label, __en - __st); \
+        fprintf(stderr, "[TIME] %s: %lf sec\n", #label, __en - __st); \
     } while(0)
 
 extern char *read_from_file(char *filepath);
